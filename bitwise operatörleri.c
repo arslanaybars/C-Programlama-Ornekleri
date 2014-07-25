66c8d5a6b38a15a92162f0643c384a68659e6fe4
Aybars <arslanaybars@gmail.com>
2014-07-25 10:10:54 +0300

    Yorum satırları eklendi
    Programın anlaşılabilir olması açısından yorum satırları eklendi.

--- bitwise operatörleri.c
+++ bitwise operatörleri.c
@@ -3,23 +3,23 @@
 #include<conio.h>
 
 
-    int x=0x1A;
-    int y=0xF2;
+    int x=0x1A;  // 0001 1010 temsil etmekte
+    int y=0xF2;  // 1111 0010 temsil etmekte
     int z;
 
 int main(void)
 {
-    z=x & y;
+    z=x & y;  // x ve y degerlerimizi and-leyerek z değişkenimize atıyoruz
+    printf("%X\n",z); // z değişkenimizin degeri
+    z=x|y;    // x ve y degelerimizi or-luyoruz
     printf("%X\n",z);
-    z=x|y;
+    z=x ^ y;  // x ve y xor
     printf("%X\n",z);
-    z=x ^ y;
+    z=x<<7;  // x'i 7 bit sola kaydırıyoruz
     printf("%X\n",z);
-    z=x<<7;
-    printf("%X\n",z);
-    z=x>>7;
-    printf("%X\n",z);
-    z=~x;
+    z=x>>7;  // x'i 7 bit saga kaydiriyoruz
+    printf("%X\n",z); 
+    z=~x;  // x'in degilini alıyoruz.
     printf("%X\n",z);
 
        fflush(stdin);
